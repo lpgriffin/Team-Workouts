aa24021c54cceffe4cb40973e863041dee33f1c8
Jerome-Li <li.jerome@yahoo.com>
2021-11-19 21:27:59 -0600

    Added Tests

--- src/Frontend.test.js
+++ src/Frontend.test.js
@@ -1,49 +1,15 @@
 import { render, getByTestId, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-/**
- * For Jerome --- PLEASE READ
- * 
- * I'm sure you might be a little confused on how to make the tests. Fear not!
- * 
- * Each testing category has a "describe statement" is comprised of 2 things:
- * - A label: Used to name the category that tests run under
- * - The tests it contains: tests that it runs (ie, its children)
- * 
- * Going further on how to make an individual test, basically just copy/paste an "it statement"
- * - Each "it statement" is a test that falls under its parent "describe statement"
- * - Each test is comprised of a label, and the test itself
- * - Some tests work differently, with "expect statements", but the majority of our tests
- *   are run by checking if an html element is rendered after an event happens, where jest tries
- *   to find the element that's supposed to be there. If it can't find it, the test throws an error 
- *   and fails. The way we grab each html element is from its "data-testid" attribute, which are 
- *   defined in the return statement of javascript files for each component 
- *   (ie App.js, login/index.js, register/index.js, etc)
- * 
- * If the stuff I described above doesn't make complete sense (it prolly wont),
- * run npm test, then when you get a prompt, type the "a" key to run all tests
- * there, the testing hierarchy should make more sense.
- * 
- * Now, not every kind of test we need for the code coverage requirements are present here.
- * The following links are excellent resources for getting started on jest
- * Jest Docs: https://jestjs.io/docs/getting-started
- * React Hooks: https://blog.logrocket.com/a-quick-guide-to-testing-react-hooks-fa584c415407/
- * 
- * Another thing to note, as of right now, our testing sucks and is not formatted correctly.
- * This is because we don't use a "setup and teardown" testing style. The jest docs have more
- * information on how to refactor our testing style to reflect, but this is something that we
- * need by iteration 2.
- * 
- * Another thing, we still need to test more than just that the html code works. We also need
- * to make sure each method is tested. For example, on the login/index.js file, we need to make
- * sure that the updateEmail and updatePassword methods work properly. How this can be tested,
- * I'm not sure, but if you're handing the tests, this has to get done too.
- * 
- * And please please PLEASE ask for help on stuff before Friday if you're confused. I would
- * 100% completely rather work on it together with you than this not get done.
- * 
- * -Lou
- */
+// Setup and teardown. These can also be inside *describe* blocks
+/*beforeEach(() => {
+  const { container } = render(<App />);
+});
+
+afterEach(() => {
+  cleanup;
+}); 
+*/
 
 describe("Jest", () => {
   it("Jest is working properly", () => {
@@ -58,12 +24,24 @@ describe("App", () => {
   });
 });
 
+// TODO
+describe("Error", () => {
+  it.skip("Back to Login Button Works", () => {
+    const { container } = render(<App />);
+    // Get to error page via setPortal or some other way
+    const errorButton = getByTestId(container, "error-backButton");
+    fireEvent.click(errorButton);
+    const login = getByTestId(container, "login");
+  });
+});
+
+
 describe("Login", () => {
   it("Login button works", () => {
     const { container } = render(<App />);
     const loginButton = getByTestId(container, "login-loginButton");
     fireEvent.click(loginButton);
-    const profile = getByTestId(container, "profile");
+    const home = getByTestId(container, "home");
   });
 
   it("Register button works", () => {
@@ -72,10 +50,65 @@ describe("Login", () => {
     fireEvent.click(registerButton);
     const register = getByTestId(container, "register");
   });
+
+  it("Email Input Updates", () => {
+    const { container } = render(<App />);
+    const emailInput = getByTestId(container, "login-emailInput");
+    fireEvent.change(emailInput, {target: {value: "linelions@e12s.com"}});
+    expect(emailInput.value).toBe("linelions@e12s.com");
+  });
+
+  it("Password Input Updates", () => {
+    const { container } = render(<App />);
+    const passwordInput = getByTestId(container, "login-passwordInput");
+    fireEvent.change(passwordInput, {target: {value: "o2y3"}});
+    expect(passwordInput.value).toBe("o2y3");
+  });
+
+  it.skip("Unable to log in with email field empty", () => {
+    const { container } = render(<App />);
+    const loginButton = getByTestId(container, "login-loginButton");
+    const emailInput = getByTestId(container, "login-emailInput");
+    fireEvent.change(emailInput, {target: {value: "wherePassword"}});
+    fireEvent.click(loginButton);
+    const login = getByTestId(container, "login");
+  });
+
+  it.skip("Unable to log in with password field empty", () => {
+    const { container } = render(<App />);
+    const loginButton = getByTestId(container, "login-loginButton");
+    const passwordInput = getByTestId(container, "login-passwordInput");
+    fireEvent.change(passwordInput, {target: {value: "thisisasecurepassword"}});
+    fireEvent.click(loginButton);
+    const login = getByTestId(container, "login");
+  });
+});
+
+describe("Home", () => {
+  it.skip("Logout button works", () => {
+    const { container } = render(<App />);
+    const loginButton = getByTestId(container, "login-loginButton"); // Modify later
+    fireEvent.click(loginButton);
+    const logoutButton = getByTestId(container, "home-logoutButton");
+    fireEvent.click(logoutButton);
+    const login = getByTestId(container, "login");
+  });
+
+  // Not implemented yet
+  it.skip("Cards", () => {
+  });
+
+  it.skip("Displays a valid userId", () => {
+    const { container } = render(<App />);
+    const loginButton = getByTestId(container, "login-loginButton");
+    fireEvent.click(loginButton);
+    const userId = getByTestId(container, "profile-subheading");
+    expect(userId.getElementsByClassName("subheading")[1]).not.toBe(undefined);
+  })
 });
 
 describe("Register", () => {
-  it("Register button works", () => {
+  it.skip("Register button works", () => {
     const { container } = render(<App />);
     const registerButton = getByTestId(container, "login-registerButton");
     fireEvent.click(registerButton);
@@ -84,7 +117,7 @@ describe("Register", () => {
     const profile = getByTestId(container, "profile");
   });
 
-  it("Back button works", () => {
+  it.skip("Back button works", () => {
     const { container } = render(<App />);
     const registerButton = getByTestId(container, "login-registerButton");
     fireEvent.click(registerButton);
@@ -92,15 +125,55 @@ describe("Register", () => {
     fireEvent.click(backButton);
     const login = getByTestId(container, "login");
   });
+
+  it("Email Input Updates", () => {
+    const { container } = render(<App />);
+    const registerButton = getByTestId(container, "login-registerButton");
+    fireEvent.click(registerButton);
+    const emailInput = getByTestId(container, "register-emailInput");
+    fireEvent.change(emailInput, {target: {value: "bigbug@dravanianforelands.com"}});
+    expect(emailInput.value).toBe("bigbug@dravanianforelands.com");
+  });
+
+  // Legacy. Kept just in case
+  it.skip("Name Input Updates", () => {
+    const { container } = render(<App />);
+    const registerButton = getByTestId(container, "login-registerButton");
+    fireEvent.click(registerButton);
+    const nameInput = getByTestId(container, "register-nameInput");
+    fireEvent.change(nameInput, {target: {value: "Ravana"}});
+    expect(nameInput.value).toBe("Ravana");
+  });
+
+  it("Username Input Updates", () => {
+    const { container } = render(<App />);
+    const registerButton = getByTestId(container, "login-registerButton");
+    fireEvent.click(registerButton);
+    const usernameInput = getByTestId(container, "register-usernameInput");
+    fireEvent.change(usernameInput, {target: {value: "Ravana"}});
+    expect(usernameInput.value).toBe("Ravana");
+  });
+
+  it("Password Input Updates", () => {
+    const { container } = render(<App />);
+    const registerButton = getByTestId(container, "login-registerButton");
+    fireEvent.click(registerButton);
+    const passwordInput = getByTestId(container, "register-passwordInput");
+    fireEvent.change(passwordInput, {target: {value: "REJOICEINTHEGLORYOFCOMBAT"}});
+    expect(passwordInput.value).toBe("REJOICEINTHEGLORYOFCOMBAT");
+  });
+
+  it("Password2 Input Updates", () => {
+    const { container } = render(<App />);
+    const registerButton = getByTestId(container, "login-registerButton");
+    fireEvent.click(registerButton);
+    const password2Input = getByTestId(container, "register-password2Input");
+    fireEvent.change(password2Input, {target: {value: "Unbending Steel"}});
+    expect(password2Input.value).toBe("Unbending Steel");
+  });
+
 });
 
-describe("Profile", () => {
-  it("Logout button works", () => {
-    const { container } = render(<App />);
-    const loginButton = getByTestId(container, "login-loginButton");
-    fireEvent.click(loginButton);
-    const logoutButton = getByTestId(container, "profile-logoutButton");
-    fireEvent.click(logoutButton);
-    const login = getByTestId(container, "login");
-  });
+// Workout page not implemented yet
+describe("Workout", () => {
 });
