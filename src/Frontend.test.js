418674d2a2bebf90a3c3c32aaba9132523c87078
Jerome-Li <li.jerome@yahoo.com>
2021-11-19 22:26:17 -0600

    Implemented Rendering Tests

--- src/Frontend.test.js
+++ src/Frontend.test.js
@@ -1,4 +1,4 @@
-import { render, getByTestId, fireEvent } from "@testing-library/react";
+import { render, getByTestId, fireEvent, screen } from "@testing-library/react";
 import App from "./App";
 
 // Setup and teardown. These can also be inside *describe* blocks
@@ -24,7 +24,7 @@ describe("App", () => {
   });
 });
 
-// TODO
+// TODO. Not really an applicable way to get to the Error Screen
 describe("Error", () => {
   it.skip("Back to Login Button Works", () => {
     const { container } = render(<App />);
@@ -35,6 +35,27 @@ describe("Error", () => {
   });
 });
 
+describe("Error Rendering", () => {
+  beforeEach(() => {
+    const { container } = render(<App />);
+    // Get to Error Screen here
+  });
+
+  it.skip("Render Heading", () => {
+    const heading = screen.getByTestId("register-heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it.skip("Render Subheading", () => {
+    const subheading = screen.getByTestId("register-subheading");
+    expect(subheading).toBeInTheDocument();
+  });
+
+  it.skip("Render Back Button", () => {
+    const backButton = screen.getByTestId("register-backButton");
+    expect(backButton).toBeInTheDocument();
+  });
+});
 
 describe("Login", () => {
   it("Login button works", () => {
@@ -84,6 +105,47 @@ describe("Login", () => {
   });
 });
 
+describe("Login Rendering", () => {
+  beforeEach(() => {
+    const { container } = render(<App />);
+  });
+
+  it("Render Logo", () => {
+    const logo = screen.getByTestId("login-logo");
+    expect(logo).toBeInTheDocument();
+  });
+
+  it("Render Email Label", () => {
+    const emailLabel = screen.getByTestId("login-emailLabel");
+    expect(emailLabel).toBeInTheDocument();
+  });
+
+  it("Render Email Input", () => {
+    const emailInput = screen.getByTestId("login-emailInput");
+    expect(emailInput).toBeInTheDocument();
+  });
+
+  it("Render Password Label", () => {
+    const passwordLabel = screen.getByTestId("login-passwordLabel");
+    expect(passwordLabel).toBeInTheDocument();
+  });
+
+  it("Render Password Input", () => {
+    const passwordInput = screen.getByTestId("login-passwordInput");
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  it("Render Login Button", () => {
+    const loginButton = screen.getByTestId("login-loginButton");
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it("Render Register Button", () => {
+    const registerButton = screen.getByTestId("login-registerButton");
+    expect(registerButton).toBeInTheDocument();
+  });
+});
+
 describe("Home", () => {
   it.skip("Logout button works", () => {
     const { container } = render(<App />);
@@ -94,17 +156,81 @@ describe("Home", () => {
     const login = getByTestId(container, "login");
   });
 
-  // Not implemented yet
-  it.skip("Cards", () => {
+  it.skip("Edit Workout", () => { // Button not implemented yet
+  }); 
+  
+  it.skip("View Workout", () => { // Button not implemented yet
+  });
+
+  it.skip("Edit Users", () => { // Button not implemented yet
+  });
+
+  it.skip("View Users", () => { // Button not implemented yet
+  });
+
+  it.skip("Cards", () => { // Not implemented yet
   });
 
   it.skip("Displays a valid userId", () => {
     const { container } = render(<App />);
     const loginButton = getByTestId(container, "login-loginButton");
     fireEvent.click(loginButton);
-    const userId = getByTestId(container, "profile-subheading");
-    expect(userId.getElementsByClassName("subheading")[1]).not.toBe(undefined);
+    const userId = getByTestId(container, "home-welcome-label");
+    expect(userId.getElementsByClassName("home-welcome-label")[1]).not.toBe(undefined);
   })
+});
+
+describe("Home Rendering", () => {
+  beforeEach(() => {
+    const { container } = render(<App />);
+    const loginButton = getByTestId(container, "login-loginButton"); // Go to Home page
+    fireEvent.click(loginButton);
+  });
+
+  it("Render Logo", () => {
+    const logo = screen.getByTestId("home-logo");
+    expect(logo).toBeInTheDocument();
+  });
+
+  it("Render Bars", () => {
+    const bars = screen.getByTestId("home-bars");
+    expect(bars).toBeInTheDocument();
+  });
+
+  it("Render Welcome Label", () => {
+    const welcomeLabel = screen.getByTestId("home-welcome-label");
+    expect(welcomeLabel).toBeInTheDocument();
+  });
+
+  it.skip("Render Level Label", () => { // Two home-level-label testIds
+    const levelLabel = screen.getByTestId("home-level-label");
+    expect(levelLabel).toBeInTheDocument();
+  });
+
+  it("Render Workout Label", () => {
+    const workoutLabel = screen.getByTestId("home-workout-label");
+    expect(workoutLabel).toBeInTheDocument();
+  });
+
+  it("Render Edit Workout Button", () => {
+    const editWorkoutButton = screen.getByTestId("home-editWorkoutButton");
+    expect(editWorkoutButton).toBeInTheDocument();
+  });
+
+  it("Render View Workout Button", () => {
+    const viewWorkoutButton = screen.getByTestId("home-viewWorkoutButton");
+    expect(viewWorkoutButton).toBeInTheDocument();
+  });
+
+  it("Render Edit Users Button", () => {
+    const editUsersButton = screen.getByTestId("home-editUsersButton");
+    expect(editUsersButton).toBeInTheDocument();
+  });
+
+  it("Render View Users Button", () => {
+    const viewUsersButton = screen.getByTestId("home-viewUsersButton");
+    expect(viewUsersButton).toBeInTheDocument();
+  });
 });
 
 describe("Register", () => {
@@ -135,8 +261,7 @@ describe("Register", () => {
     expect(emailInput.value).toBe("bigbug@dravanianforelands.com");
   });
 
-  // Legacy. Kept just in case
-  it.skip("Name Input Updates", () => {
+  it.skip("Name Input Updates", () => { // Legacy. Kept just in case
     const { container } = render(<App />);
     const registerButton = getByTestId(container, "login-registerButton");
     fireEvent.click(registerButton);
@@ -174,6 +299,63 @@ describe("Register", () => {
 
 });
 
-// Workout page not implemented yet
-describe("Workout", () => {
+describe("Register Rendering", () => {
+  beforeEach(() => {
+    const { container } = render(<App />);
+    const registerButton = getByTestId(container, "login-registerButton");
+    fireEvent.click(registerButton);
+  });
+
+  it("Render Logo", () => {
+    const logo = screen.getByTestId("register-logo");
+    expect(logo).toBeInTheDocument();
+  });
+
+  it("Render Email Label", () => {
+    const emailLabel = screen.getByTestId("register-emailLabel");
+    expect(emailLabel).toBeInTheDocument();
+  });
+
+  it("Render Email Input", () => {
+    const emailInput = screen.getByTestId("register-emailInput");
+    expect(emailInput).toBeInTheDocument();
+  });
+
+  it("Render Username Label", () => {
+    const usernameLabel = screen.getByTestId("register-usernameLabel");
+    expect(usernameLabel).toBeInTheDocument();
+  });
+
+  it("Render Username Input", () => {
+    const usernameInput = screen.getByTestId("register-usernameInput");
+    expect(usernameInput).toBeInTheDocument();
+  });
+
+  it("Render Password Label", () => {
+    const passwordLabel = screen.getByTestId("register-passwordLabel");
+    expect(passwordLabel).toBeInTheDocument();
+  });
+
+  it("Render Password Input", () => {
+    const passwordInput = screen.getByTestId("register-passwordInput");
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  it("Render Pasword2 Label", () => {
+    const password2Label = screen.getByTestId("register-password2Label");
+    expect(password2Label).toBeInTheDocument();
+  });
+
+  it("Render Password2 Input", () => {
+    const password2Input = screen.getByTestId("register-password2Input");
+    expect(password2Input).toBeInTheDocument();
+  });
+
+  it("Render Register Button", () => {
+    const registerButton = screen.getByTestId("register-registerButton");
+    expect(registerButton).toBeInTheDocument();
+  });
+});
+
+describe("Workout", () => { // Workout page not implemented yet
 });
